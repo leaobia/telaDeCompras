91a7b11d6d50d316bb0d4ca3c1c603a26248b75a
Bianca Leão <biancapeleao@gmail.com>
2024-08-15 08:57:54 -0300

    inicio ao css

--- FrontEnd/e-catalogos/src/components/ProductList.jsx
+++ FrontEnd/e-catalogos/src/components/ProductList.jsx
@@ -25,67 +25,88 @@ function ProductList() {
               ...prevImages,
               [produtoAtual.id]: imageData.images
             }));
-            setImage(imageData.images[0].path); 
+            setImage(imageData.images[0].path);
           });
-          fetch(`http://localhost:3000/v1/eCatalogos/skus/${produtoAtual.id}`)
+        fetch(`http://localhost:3000/v1/eCatalogos/skus/${produtoAtual.id}`)
           .then(response => response.json())
           .then(skusData => {
-           console.log(skusData);
+            console.log(skusData);
           });
       } else {
         setImage(images[produtoAtual.id][0].path);
       }
     }
   }, [paginaAtual, produtos, images]);
-
   const proximaPagina = () => {
-    if ((paginaAtual + 1) * produtosPorPagina < produtos.length) {
+    if ((paginaAtual + 1) * produtosPorPagina >= produtos.length) {
+      setPaginaAtual(0);
+    } else {
       setPaginaAtual(paginaAtual + 1);
     }
   };
-
   const paginaAnterior = () => {
-    if (paginaAtual > 0) {
+    if (paginaAtual === 0) {
+      setPaginaAtual(Math.ceil(produtos.length / produtosPorPagina) - 1);
+    } else {
       setPaginaAtual(paginaAtual - 1);
     }
   };
+  
 
   const produtoAtual = produtos[paginaAtual];
-
   return (
-    <div>
+    <div className='container'>
+      <header className='container__header'>
+        <div className="container__header--categoria">
+        Categoria Name
+        </div>
+      </header>
       {produtoAtual && (
-        <div key={produtoAtual.id}>
-          <h3>{produtoAtual.name}</h3>
-          <p>{produtoAtual.category}</p>
-          <p>{produtoAtual.price}</p>
-
+        <div className='container__produto'>
           {images[produtoAtual.id] && images[produtoAtual.id].length > 0 && (
-            <>
-              <div>
-                <img src={image}alt="imagem" />
+            <div className='container__imagens'>
+              <div className="container__imagem-grande" style={{ backgroundImage: `url(${image})` }}>
+                <button className="container__botao container__botao--anterior" onClick={paginaAnterior}>
+                &larr;
+                </button>
+                <button
+                  className="container__botao container__botao--proxima"
+                  onClick={proximaPagina}
+                >
+                &rarr;
+                </button>
+              </div>
+  
+              <div className='container__troca-imagem'>
                 {images[produtoAtual.id].map((img, index) => (
-                  <button key={index} onClick={() => setImage(img.path)}>
-                    <img src={img.path} alt={`Thumbnail ${index + 1}`} style={{ width: 50, height: 50 }} />
+                  <button
+                    key={index}
+                    className="container__thumbnail"
+                    onClick={() => setImage(img.path)}
+                  >
+                    <img
+                      src={img.path}
+                      alt={`Thumbnail ${index + 1}`}
+                      className="container__thumbnail-img"
+                      style={{ width: 50, height: 50 }}
+                    />
                   </button>
                 ))}
               </div>
-            </>
+            </div>
           )}
+          <div className="container__dados">
+            botao, nome, referencia, preco
+          </div>
+          <div className="container__pack">
+            <div className="container__pack-display">aaaaaaaaaaaaaaaaaaaaaaa</div>
+            <div className="container__pack-quantity-selector">eeeeeeeeeeeeeeeeeee</div>
+          </div>
         </div>
       )}
-
-      <button onClick={paginaAnterior} disabled={paginaAtual === 0}>
-        Anterior
-      </button>
-      <button
-        onClick={proximaPagina}
-        disabled={(paginaAtual + 1) * produtosPorPagina >= produtos.length}
-      >
-        Próxima
-      </button>
     </div>
   );
-}
+  
 
+}
 export default ProductList;
